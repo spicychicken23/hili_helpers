0f3802e9b773bee461390e79bdb84a67c9fb7c58
ahmdhfz14 <ahmadtorres14@gmail.com>
2024-06-12 18:53:03 +0800

    minor update

--- lib/pages/services_page.dart
+++ lib/pages/services_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:hili_helpers/components/home.dart';
 import 'package:hili_helpers/components/services.dart';
 import 'package:hili_helpers/models/servicesLists.dart';
